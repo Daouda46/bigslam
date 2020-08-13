9337375c150e543567ebd085edb5c7a61e42bc9b
Daouda Ouattara <60108202+Daouda46@users.noreply.github.com>
2020-08-13 01:52:13 +0000

    base, blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def page_home(request):
+    return render(request, 'pages/blog/index.html')
+
+def single(request):
+    return render(request, 'pages/blog/single.html')
+
--- team/urls.py
+++ team/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     
     path('', views.home_page, name='home'),
-    path('calendrier', views.shedule, name='shedule'),
+    path('calendrier', views.schedule, name='shedule'),
     path('our-team', views.our_team, name='our_team'),
     path('contact', views.contact, name='contact'),
 ]
